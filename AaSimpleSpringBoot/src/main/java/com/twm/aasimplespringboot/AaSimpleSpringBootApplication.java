3371818a57ed8cc31d4ea15bbf0809fe6849f543
wenming_tang <twm951226@126.com>
2021-05-05 12:58:24 +0800

    a simple SpringBoot project, must add web starter can running.

---
+++ AaSimpleSpringBoot/src/main/java/com/twm/aasimplespringboot/AaSimpleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.twm.aasimplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaSimpleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaSimpleSpringBootApplication.class, args);
+    }
+
+}
---
+++ AaSimpleSpringBoot/src/test/java/com/twm/aasimplespringboot/AaSimpleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twm.aasimplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaSimpleSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
